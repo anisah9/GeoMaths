23ee3ad465a103f1d7cf8888238c6b9e07fe9933
anisah9 <123170553+anisah9@users.noreply.github.com>
2023-12-03 11:25:09 +0000

    Created an angle.py page
    All angles and triangles tools will go here

---
+++ angle.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+
+class AngleTool(tk.Frame):
+    def __init__(self, master):
+        tk.Frame.__init__(self, master, bg="#ecf0f1")
+        self.winfo_toplevel().title("Angle Tool")
+
