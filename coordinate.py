09b710a3c3035235c0984df856f2cb7e25f79c88
anisah9 <123170553+anisah9@users.noreply.github.com>
2024-04-03 10:59:50 +0100

    Update coordinate.py
    Tab for Reflect Shapes added

--- coordinate.py
+++ coordinate.py
@@ -5,6 +5,7 @@ from home_tab import HomeTab
 from full_grid_tab import FullGridTab
 from coord_shape_tab import CoordShapeTab
 from translate_shape_tab import TranslateShapeTab
+from reflect_shape_tab import ReflectShapeTab
 
 
 
@@ -18,16 +19,17 @@ class CoordinateTool(tk.Frame):
         self.full_grid_tab = FullGridTab(self.tab_control)  # Pass the instance of CoordinateTool
         self.coord_shape_tab = CoordShapeTab(self.tab_control)
         self.translate_shape_tab = TranslateShapeTab(self.tab_control)
+        self.reflect_shape_tab = ReflectShapeTab(self.tab_control)
 
         self.tab_control.add(self.home_tab, text='Home')
         self.tab_control.add(self.full_grid_tab, text='Full Coordinate Grid')
         self.tab_control.add(self.coord_shape_tab, text= 'Coordinate Shapes')
         self.tab_control.add(self.translate_shape_tab, text='Translate Shapes')
+        self.tab_control.add(self.reflect_shape_tab, text='Reflect Shapes')
         
 
         self.tab_control.pack(expand=1, fill="both")
 
-# Below this, you could have your application startup logic:
 if __name__ == "__main__":
     root = tk.Tk()
     app = CoordinateTool(root)
