f55235bc520e3cb8e3dd01e016c92112f139cb20
anisah9 <123170553+anisah9@users.noreply.github.com>
2024-04-07 10:11:29 +0100

    Create home_angle_tab.py
    Create home_angle_tab.py page

---
+++ home_angle_tab.py
@@ -0,0 +1,8 @@
+# home_angle_tab.py
+import tkinter as tk
+
+class HomeTab(tk.Frame):
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
+        label = tk.Label(self, text="Welcome to the Home Tab")
+        label.pack()
